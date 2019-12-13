b38762bdfd4de1579da52b5f84a21dd5ef272d4e
hammiche <kamelhammiche44@gmail.com>
2019-12-13 16:48:04 +0100

    add creation fichier.py

--- exercice2/main.py
+++ exercice2/main.py
@@ -3,7 +3,7 @@ from bus import Bus
 
 if __name__ == '__main__':
 
-    bus1 = Bus("AA-000-ZZ", "yellow",2)
+    bus1 = Bus("AA-000-ZZ", "yellow",4)
     print("le bus immatriculé {} , couleur {} à {} étages".format(bus1.immatriculation,bus1.couleur,bus1.number_floors))
 
     voiture1 = Voiture("BG-564-NB", "white", 5)
